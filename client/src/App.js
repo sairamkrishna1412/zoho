9651af44d8ce3b8f767e6ea6814271a5e747e3c3
sai ram krishna <sairamkrishna1412@gmail.com>
2022-03-10 13:52:17 +0530

    Add contact implemented and inital frontend setup done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
 import './App.css';
 
 function App() {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    async function getContacts() {
+      const contacts = await axios.get('/api/contacts');
+      setContacts(contacts);
+    }
+    getContacts();
+  }, [setContacts]);
+  console.log(contacts);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" exact element={<Home></Home>}></Route>
+        <Route path="/signup" element={<Signup></Signup>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
     </div>
   );
 }
